53891a182e3b48261b6928b6e2470aa5ef1e701a
Smelly Cat <smelly@c.at>
2019-04-07 20:34:37 +1000

    Change board as a map from Rank to Pieces. Pieces are a map from Cell Name to Piece. Cell Name is a string of File and Rank, e.g., b5

--- src/chess/board.clj
+++ src/chess/board.clj
@@ -2,14 +2,20 @@
   (:use [chess.core :only [ascii-to-string]]
         [chess.piece :only [empty-piece]]))
 
-(defn- a-h []
-  (map ascii-to-string (range 97 105)))
-
-(defn- cell [rank]
-  (->> (a-h)
-       (map (fn [file] {(str file rank) (empty-piece file rank)}))))
+(def files (map ascii-to-string (range 97 105)))
 
 (def ranks (range 1 9))
 
+(defn- cells [rank]
+  (let [ranks            (take 8 (repeat rank))
+        files-ranks      (map vector files ranks)
+        file             (fn [file-rank] (nth file-rank 0))
+        file-rank-str    (fn [file-rank] (str (file file-rank) rank))]
+    (reduce
+     (fn [m file-rank]
+       (assoc m (file-rank-str file-rank) (empty-piece (file file-rank) rank)))
+     {}
+     files-ranks)))
+
 (defn board []
-  (reduce (fn [m k] (assoc m k (cell k))) {} ranks))
+  (reduce (fn [m rank] (assoc m rank (cells rank))) {} ranks))
