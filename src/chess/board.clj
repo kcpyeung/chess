7c48cc03e83956f9d739199facbac0ce5a8660b9
Smelly Cat <smelly@c.at>
2019-04-08 21:05:34 +1000

    Refactor board to make-ranks and make-files. Use only file letter as key

--- src/chess/board.clj
+++ src/chess/board.clj
@@ -6,16 +6,10 @@
 
 (def ranks (range 1 9))
 
-(defn- cells [rank]
-  (let [ranks            (take 8 (repeat rank))
-        files-ranks      (map vector files ranks)
-        file             (fn [file-rank] (nth file-rank 0))
-        file-rank-str    (fn [file-rank] (str (file file-rank) rank))]
-    (reduce
-     (fn [m file-rank]
-       (assoc m (file-rank-str file-rank) (empty-piece (file file-rank) rank)))
-     {}
-     files-ranks)))
+(defn- make-files [rank]
+  (reduce (fn [m file] (assoc m file (empty-piece file rank))) {} files))
 
-(defn board []
-  (reduce (fn [m rank] (assoc m rank (cells rank))) {} ranks))
+(defn- make-ranks []
+  (reduce (fn [m rank] (assoc m rank (make-files rank))) {} ranks))
+
+(def board make-ranks)
