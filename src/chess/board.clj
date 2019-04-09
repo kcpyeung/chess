5f732a1907399546d33788e92d9e5281bc8e092c
Smelly Cat <smelly@c.at>
2019-04-09 21:27:49 +1000

    Put black king on board initialisation

--- src/chess/board.clj
+++ src/chess/board.clj
@@ -4,7 +4,7 @@
 
 (def files (map ascii-to-string (range 97 105)))
 
-(def ranks (range 8 0 -1))
+(def ranks (map str (range 8 0 -1)))
 
 (defn- make-files [rank]
   (reduce (fn [m file] (assoc m file (empty-piece file rank))) {} files))
@@ -12,4 +12,15 @@
 (defn- make-ranks []
   (reduce (fn [m rank] (assoc m rank (make-files rank))) {} ranks))
 
-(def board make-ranks)
+(defn- place-piece [piece board]
+  (let [rank-of-piece            (:rank piece)
+        file-of-piece            (:file piece)
+        rank-containing-piece    (board rank-of-piece)]
+    (->> {file-of-piece piece}
+         (merge rank-containing-piece)
+         (assoc {} rank-of-piece)
+         (merge board))))
+
+(defn board [piece-maker]
+  (let [bk (first (piece-maker "king" "black"))]
+    (place-piece bk (make-ranks))))
--- test/chess/board_test.clj
+++ test/chess/board_test.clj
@@ -1,11 +1,18 @@
 (ns chess.board-test
   (:require [clojure.test :refer :all]
-            [chess.board :refer :all]))
+            [chess.board :refer [board]]
+            [chess.piece-maker :refer [get-piece-maker]]))
 
 (deftest make-new-board
-  (let [board (board)]
+  (let [board (board (get-piece-maker 1))]
     (testing "has 8 ranks"
              (is (= 8 (count board))))
 
     (testing "every rank has 8 files"
              (is (every? #(= 8 %) (map #(count (nth % 1)) board))))))
+
+(deftest placing-pieces-on-board
+  (let [new-board (board (get-piece-maker 1))
+        f1        ((new-board "1") "f")]
+    (testing "board places black king on at f1 itself using Piece Maker"
+             (is (= f1 {:name "king", :colour "black", :file "f", :rank "1", :sym "k"})))))
