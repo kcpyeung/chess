8d7074959532f584777e4de8bdb1e0c72e5b6b36
Smelly Cat <smelly@c.at>
2019-04-14 08:51:27 +1000

    Refactor rank/file adjacency logic into functions for better readability

--- src/chess/no_adjacent_kings_rules.clj
+++ src/chess/no_adjacent_kings_rules.clj
@@ -8,35 +8,45 @@
 
 (defn- no-kings-in-adjacent-ranks-of-the-same-file [board]
   (let [[king1 king2] (capture-kings board)]
-    (not
-     (and
-      king1
-      king2
-      (->> [king1 king2]
-           (map :rank)
-           (map (fn [rank-keyword] (Integer/parseInt (name rank-keyword))))
-           (apply -)
-           Math/abs
-           (= 1))
-      (->> [king1 king2]
-           (map :file)
-           (apply =))))))
+    (letfn
+      [(in-adjacent-ranks []
+                          (->> [king1 king2]
+                               (map :rank)
+                               (map (fn [rank-keyword] (Integer/parseInt (name rank-keyword))))
+                               (apply -)
+                               Math/abs
+                               (= 1)))
+       (in-same-file []
+                     (->> [king1 king2]
+                          (map :file)
+                          (apply =)))]
+      (not
+       (and
+        king1
+        king2
+        (in-adjacent-ranks)
+        (in-same-file))))))
 
 (defn- no-kings-in-adjacent-files-of-the-same-rank [board]
   (let [adjacent-files [#{:a :b} #{:b :c} #{:c :d} #{:d :e} #{:e :f} #{:f :g} #{:g :h}]
         [king1 king2]  (capture-kings board)]
-    (not
-     (and
-      king1
-      king2
-      (as-> [king1 king2] v
-            (map :file v)
-            (filter (fn [neighbours] (= neighbours (into #{} v))) adjacent-files)
-            (not-every? empty? v))
-      (->> [king1 king2]
-           (map :rank)
-           (map (fn [rank-keyword] (Integer/parseInt (name rank-keyword))))
-           (apply =))))))
+    (letfn
+      [(in-adjacent-files []
+                          (as-> [king1 king2] v
+                                (map :file v)
+                                (filter (fn [neighbours] (= neighbours (into #{} v))) adjacent-files)
+                                (not-every? empty? v)))
+       (in-same-rank []
+                     (->> [king1 king2]
+                          (map :rank)
+                          (map (fn [rank-keyword] (Integer/parseInt (name rank-keyword))))
+                          (apply =)))]
+      (not
+       (and
+        king1
+        king2
+        (in-adjacent-files)
+        (in-same-rank))))))
 
 (defn no-adjacent-kings? [board]
   (and (no-kings-in-adjacent-files-of-the-same-rank board)
