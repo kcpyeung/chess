4f97f5bb8384e193f0c57d96692fca62b8d6989d
Smelly Cat <smelly@c.at>
2019-04-13 15:56:32 +1000

    Board is valid if no kings in adjacent ranks in the same file

--- src/chess/placement_rules.clj
+++ src/chess/placement_rules.clj
@@ -1,7 +1,20 @@
-(ns chess.placement-rules
-  (:require [chess.core :refer [ascii-to-keyword]]))
+(ns chess.placement-rules)
 
-(defn- no-adjacent-kings [board]
+(defn- no-kings-in-adjacent-ranks-of-the-same-file [board]
+  (->>
+   (reduce
+    (fn [acc [_ files]]
+      (map (fn [[_ piece]] (if (= :king (:name piece)) (conj acc piece))) files))
+    [] board)
+   flatten
+   (map :rank)
+   (map (fn [rank-keyword] (Integer/parseInt (name rank-keyword))))
+   (apply -)
+   Math/abs
+   (= 1)
+   not))
+
+(defn- no-kings-in-adjacent-files [board]
   (let [adjacent-files [[:a :b] [:b :c] [:c :d] [:d :e] [:e :f] [:f :g] [:g :h]]]
     (->>
      (mapcat
@@ -13,4 +26,5 @@
      (not-any? true?))))
 
 (defn valid? [board]
-  (no-adjacent-kings board))
+  (and (no-kings-in-adjacent-files board)
+       (no-kings-in-adjacent-ranks-of-the-same-file board)))
--- test/chess/placement_rules_test.clj
+++ test/chess/placement_rules_test.clj
@@ -3,12 +3,25 @@
             [chess.placement-rules :refer [valid?]]))
 
 (deftest no-adjacent-kings
-  (testing "invalid if kings are in adjacent files in the same rank"
-           (let [board        {:6 {:a {:name :king, :colour :white, :rank :6, :file :a, :sym :K}
-                                   :b {:name :king, :colour :black, :rank :6, :file :b, :sym :k}}}]
-             (is (not (valid? board)))))
+  (testing "adjacent files"
+           (testing "invalid if kings are in adjacent files in the same rank"
+                    (let [board        {:6 {:a {:name :king, :colour :white, :rank :6, :file :a, :sym :K}
+                                            :b {:name :king, :colour :black, :rank :6, :file :b, :sym :k}}}]
+                      (is (not (valid? board)))))
 
-  (testing "valid if kings are separated by at least 1 file in the same rank"
-           (let [board        {:6 {:a {:name :king, :colour :white, :rank :6, :file :a, :sym :K}
-                                   :c {:name :king, :colour :black, :rank :6, :file :c, :sym :k}}}]
-             (is (valid? board)))))
+           (testing "valid if kings are separated by at least 1 file in the same rank"
+                    (let [board        {:6 {:a {:name :king, :colour :white, :rank :6, :file :a, :sym :K}
+                                            :c {:name :king, :colour :black, :rank :6, :file :c, :sym :k}}}]
+                      (is (valid? board)))))
+
+  (testing "adjacent ranks"
+           (testing "invalid if kings are in adjacent ranks in the same file"
+                    (let [board        {:6 {:a {:name :king, :colour :white, :rank :6, :file :a, :sym :K}}
+                                        :7 {:a {:name :king, :colour :black, :rank :7, :file :a, :sym :k}}}]
+                      (is (not (valid? board)))))
+
+           (testing "valid if kings are separated by at least 1 rank in the same file"
+                    (let [board        {:6 {:a {:name :king, :colour :white, :rank :6, :file :a, :sym :K}}
+                                        :8 {:a {:name :king, :colour :black, :rank :8, :file :a, :sym :k}}}]
+                      (is (valid? board))))))
+
